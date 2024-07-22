3c2217b5693b89972abf99a0d36ed08efb6a541b
lezernack <luks.ezck@gmail.com>
2024-07-22 14:13:52 -0600

    added a navbar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -11,8 +11,9 @@ const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-              Hello, I'm(" ")
+              Hello, I'm{" "}
             </span>
+            <br />
             <TypeAnimation
               sequence={[
                 "Lukas",
@@ -26,7 +27,6 @@ const HeroSection = () => {
               ]}
               wrapper="span"
               speed={50}
-              style={{ fontSize: "2rem", display: "inline-block" }}
               repeat={Infinity}
             />
           </h1>
