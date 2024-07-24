0593f43a88228abe99a076affa5fe46f61ac81d5
lezernack <luks.ezck@gmail.com>
2024-07-23 18:55:27 -0600

    started putting in projects

---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectCard from "./ProjectCard.jsx";
+
+const ProjectsSection = () => {
+  return (
+    <>
+      <h2>My Projects</h2>
+    </>
+  );
+};
+
+export default ProjectsSection;
