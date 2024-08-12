bce726ae1d83010a55a6571652530ba3b3427918
lezernack <luks.ezck@gmail.com>
2024-08-12 12:25:46 -0600

    changed ' to &apos;

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -46,11 +46,13 @@ const EmailSection = () => {
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">
+          Let&apos;s Connect
+        </h5>
         <p className="text-[#ADB7DE] mb-4 max-w-md">
           {" "}
-          I'm currently looking for new possibilites, my inbox is always open.
-          I'll do my best to get back to you.
+          I&apos;m currently looking for new possibilites, my inbox is always
+          open. I&apos;ll do my best to get back to you.
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/lezernack" target="_blank">
